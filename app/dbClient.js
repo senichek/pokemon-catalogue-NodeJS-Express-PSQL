016fcfcb2c03e2f46feed14ea7a7a3a5e8b69069
senichek <senichek@gmail.com>
2023-01-07 22:38:13 +0100

    added ssl require true to db config

--- app/dbClient.js
+++ app/dbClient.js
@@ -4,7 +4,9 @@ const { Client } = require("pg");
 
 const client = new Client({
     connectionString: process.env.PG_URL,
-    ssl: { rejectUnauthorized: false }
+    ssl: { 
+        require: true,
+        rejectUnauthorized: false }
   });
 
 client.connect((error) => {
